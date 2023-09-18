ef18119d223755e5e56ba4b91cf12a1cf3fcd3fe
David Hernández <davidh@MacBook-Pro-de-David.local>
2023-09-18 12:59:49 -0600

    test button removed from login page

--- pages/login.vue
+++ pages/login.vue
@@ -39,9 +39,7 @@ const login = async () => {
       clas
       s="flex flex-col mx-auto items-center justify-center space-y-4 shadow-2xl w-80 h-[450px] rounded-[30px]"
     >
-    <button @click="test()">
-    Test
-  </button>
+
       <div class="flex flex-col items-center justify-center">
         <nuxt-img src="logo.png" width="100px" class="mx-auto" />
         <span class="text-xl font-bold">Ejuri</span>
